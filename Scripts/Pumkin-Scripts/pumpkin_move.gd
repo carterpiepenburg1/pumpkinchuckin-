36a584fada28279e8e717bad390bf0a4f7f8aa63
Carter Piepenburg <piepen22@uwm.edu>
2025-11-24 01:16:57 -0600

    Made it look pretty

--- Scripts/Pumkin-Scripts/pumpkin_move.gd
+++ Scripts/Pumkin-Scripts/pumpkin_move.gd
@@ -5,11 +5,17 @@ extends Node3D
 @onready var inHand = false
 @onready var inSling = false
 
+@onready var velocity = 0
+
 func _process(delta: float) -> void:
 	
 	if onVine == false && inHand == false && inSling == false:
-		global_position.y -= delta * 3
+		velocity += 2.6 * delta
+		global_position.y -= velocity * delta
 		
-		if global_position.y < -1:
+		if global_position.y < 0:
 			queue_free()
 			print("Pumpkin has been Dropped")
+	
+	else:
+		velocity = 0
