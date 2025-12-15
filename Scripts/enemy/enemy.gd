4b2fccf6e4bdde57b17cec3f1fc752311c9620dd
IzacNienhaus <izac@uwm.edu>
2025-12-15 14:56:32 -0600

    Now Im Finished

--- Scripts/enemy/enemy.gd
+++ Scripts/enemy/enemy.gd
@@ -38,10 +38,10 @@ func _on_area_entered(area: Area3D) -> void:
 		Globals.score += parent.pointValue
 		
 		var newPointLabel = pointLabel.instantiate()
-		newPointLabel.global_position = parent.global_position
 		newPointLabel.points = parent.pointValue
-		newPointLabel.rotation.y = deg_to_rad(270)
 		get_tree().current_scene.add_child(newPointLabel)
+		newPointLabel.global_position = parent.global_position
+		newPointLabel.rotation.y = deg_to_rad(270)
 		
 		queue_free()
 
